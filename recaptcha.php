53d2864e789a49cf3d20ae07728ad4dff094b9d8
enyahslezah <33740473+enyahslezah@users.noreply.github.com>
2017-11-19 17:47:40 +0800

    Update recaptcha.php
    request para macheck kung ano yung dinagdag ata to hahahaha

--- recaptcha.php
+++ recaptcha.php
@@ -1,5 +1,5 @@
 <?php
-
+//hi guys pag nabasa niyo to ibig sabihin successful
 // To use reCAPTCHA, you need to sign up for an API key pair for your site.
 // link: http://www.google.com/recaptcha/admin
 $config['recaptcha_site_key'] = 'XXXXXXXXXXXXXXXXXXXXYOUR SITE KEY HEREXXXXXXXXXXXXXXXXXXXXX';
